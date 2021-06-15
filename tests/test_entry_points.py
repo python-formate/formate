be0aa16ba34ed17459352bbc45fc26dd3b22e914
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-06-15 22:52:06 +0100

    Add tests that entry points are correctly registered.

---
+++ tests/test_entry_points.py
@@ -0,0 +1,34 @@
+# 3rd party
+from coincidence import AdvancedDataRegressionFixture
+import entrypoints
+
+# These tests will fail if not installed and just running straight from source
+
+
+def test_entry_points():
+	entry_points = entrypoints.get_group_all("formate_hooks")
+
+	print(entry_points)
+
+	hook_names = {e.name for e in entry_points}
+
+	if "black" in hook_names:
+		hook_names.remove("black")
+
+	assert hook_names == {
+			"reformat-generics",
+			"dynamic_quotes",
+			"noqa_reformat",
+			"squish_stubs",
+			"ellipsis_reformat",
+			"collections-import-rewrite",
+			"isort",
+			"yapf",
+			}, hook_names
+
+
+def test_entry_points_file(advanced_data_regression: AdvancedDataRegressionFixture):
+	for config, distro in entrypoints.iter_files_distros():
+		if distro.name == "formate":
+			advanced_data_regression.check(dict(config["formate_hooks"]))
+		break
