801fc8c63747228ce719d3d4b9fa93999fc0f916
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-01-30 14:39:58 +0000

    Bump version v0.7.0 -> v0.8.0

--- formate/__init__.py
+++ formate/__init__.py
@@ -49,7 +49,7 @@ from formate.utils import _find_from_parents, syntaxerror_for_file
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020-2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.7.0"
+__version__: str = "0.8.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ("call_hooks", "reformat_file", "Reformatter", "isort_hook", "yapf_hook")
